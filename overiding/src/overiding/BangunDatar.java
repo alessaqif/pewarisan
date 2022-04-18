f185fce196b27608ec2bda9047e46eb6a2124c9f
alessaqif <101534769+alessaqif@users.noreply.github.com>
2022-04-18 13:28:15 +0700

    Add files via upload

---
+++ overiding/src/overiding/BangunDatar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overiding;
+
+/**
+ *
+ * @author ACER
+ */
+public class BangunDatar {
+    
+    public void luas(){
+        System.out.println("menghitung luas lingkaran");
+        
+    }
+    public void keliling(){
+        System.out.println("menghitung keliling lingkaran");
+        
+    }
+    
+}
+
+    
+
